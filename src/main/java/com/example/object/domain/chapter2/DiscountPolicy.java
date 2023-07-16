3e95d2710995884864e8bb7f6d05fc640cb66d34
Yohwan Lim <dyghks7102@naver.com>
2023-07-16 14:37:08 +0900

    feat. 차이에 의한 프로그래밍
    상속을 이용해 다른 부분만을 추가하여 새로운 클래스를 쉽고 빠르게 만들 수 있음

--- src/main/java/com/example/object/domain/chapter2/DiscountPolicy.java
+++ src/main/java/com/example/object/domain/chapter2/DiscountPolicy.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public abstract class DiscountPolicy {
+    // 차이에 의한 프로그래밍 = 부모 클래스와 다른 부분만을 추가하여 새로운 클래스를 쉽고 빠르게 만드는 방법
     private List<DiscountCondition> conditions = new ArrayList<>();
 
     public DiscountPolicy(DiscountCondition ... conditions) {
