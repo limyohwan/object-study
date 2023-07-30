765ff3ab738477bc26dd2df66eb415f8a13152c5
Yohwan Lim <dyghks7102@naver.com>
2023-07-30 20:22:36 +0900

    feat. Rectangle 예제 클래스 생성
    코드 중복 발생할 확률 높음
    변경에 취약함, right, bottom 대신 length, height를 이용해 사각형 표현을 변경하면 기존 메서드를 사용하던 코드에 모두 영향을 미침

---
+++ src/main/java/com/example/object/domain/chapter4/Rectangle.java
@@ -0,0 +1,55 @@
+package com.example.object.domain.chapter4;
+
+public class Rectangle {
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+
+    public Rectangle(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+
+    class AnyClass {
+        // 너비와 높이를 증가시키는 코드
+        void anyMethod(Rectangle rectangle, int multiple) {
+            rectangle.setRight(rectangle.getRight() * multiple);
+            rectangle.setBottom(rectangle.getBottom() * multiple);
+        }
+    }
+}
