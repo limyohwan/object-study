c5e41402b6c3a0840d7ed6eb1dc4ca8055974380
Yohwan Lim <dyghks7102@naver.com>
2023-07-30 20:27:22 +0900

    feat. ReservationAgency로 새어나간 데이터의 책임을 실제 데이터를 포함하는 DiscountCondition 객체로 이동

--- src/main/java/com/example/object/domain/chapter4/DiscountCondition.java
+++ src/main/java/com/example/object/domain/chapter4/DiscountCondition.java
@@ -14,6 +14,24 @@ public class DiscountCondition {
         return type;
     }
 
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if(type != DiscountConditionType.PERIOD) {
+            throw new IllegalStateException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+                this.startTime.compareTo(time) <= 0 &&
+                this.endTime.compareTo(time) >= 0;
+    }
+
+    public boolean isDiscountable(int sequence) {
+        if(type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalStateException();
+        }
+
+        return this.sequence == sequence;
+    }
+
     public void setType(DiscountConditionType type) {
         this.type = type;
     }
