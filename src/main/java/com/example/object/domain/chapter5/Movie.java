de720b872d0107d6b8eacfdd42694771e1f76a60
Yohwan Lim <dyghks7102@naver.com>
2023-08-07 22:26:20 +0900

    feat. 할인 조건처럼 Movie객체도 다형성 패턴을 사용하여 개선할 수 있음
    위와같은 형태는 새로운 할인 조건이 생길 때마다 클래스를 계속해서 추가해 주어야하는 단점이 있음
    이를 보완하기 위해 나온것이 합성임

--- src/main/java/com/example/object/domain/chapter5/Movie.java
+++ src/main/java/com/example/object/domain/chapter5/Movie.java
@@ -5,17 +5,25 @@ import com.example.object.domain.chapter2.Money;
 import java.time.Duration;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
 //    private List<OldDiscountCondition> discountConditions;
     private List<DiscountCondition> discountConditions;
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
+//    private MovieType movieType;
+//    private Money discountAmount;
+//    private double discountPercent;
 //    private List<PeriodCondition> periodConditions;
 //    private List<SequenceCondition> sequenceConditions;
+
+
+    public Movie(String title, Duration runningTime, Money fee, List<DiscountCondition> discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = discountConditions;
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if(isDiscountable(screening)) {
@@ -48,28 +56,34 @@ public class Movie {
 //                .anyMatch(condition -> condition.isSatisfiedBy(screening));
 //    }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
+    abstract protected Money calculateDiscountAmount();
 
-        throw new IllegalStateException();
+    protected Money getFee() {
+        return fee;
     }
 
-    private Money calculateAmountDiscountAmount(){
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
+//    private Money calculateDiscountAmount() {
+//        switch (movieType) {
+//            case AMOUNT_DISCOUNT:
+//                return calculateAmountDiscountAmount();
+//            case PERCENT_DISCOUNT:
+//                return calculatePercentDiscountAmount();
+//            case NONE_DISCOUNT:
+//                return calculateNoneDiscountAmount();
+//        }
+//
+//        throw new IllegalStateException();
+//    }
+//
+//    private Money calculateAmountDiscountAmount(){
+//        return discountAmount;
+//    }
+//
+//    private Money calculatePercentDiscountAmount() {
+//        return fee.times(discountPercent);
+//    }
+//
+//    private Money calculateNoneDiscountAmount() {
+//        return Money.ZERO;
+//    }
 }
