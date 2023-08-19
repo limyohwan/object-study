2da6dc116583050280e612636beac096aaf8e7b5
Yohwan Lim <dyghks7102@naver.com>
2023-08-19 23:57:18 +0900

    feat. 반복일정 클래스와 간단한 사용법 정리

--- src/main/java/com/example/object/domain/chapter6/EventMain.java
+++ src/main/java/com/example/object/domain/chapter6/EventMain.java
@@ -1,10 +1,14 @@
 package com.example.object.domain.chapter6;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class EventMain {
     public static void main(String[] args) {
         Event meeting = new Event("회의", LocalDateTime.of(2019, 5, 8, 10, 30), Duration.ofMinutes(30L));
+
+        RecurringSchedule schedule = new RecurringSchedule("회의", DayOfWeek.WEDNESDAY, LocalTime.of(10, 30), Duration.ofMinutes(30L));
     }
 }
