2a2c725f5a942857ee8fe13a96abf3797f2d6446
Yohwan Lim <dyghks7102@naver.com>
2023-08-20 23:33:11 +0900

    feat. isSatisfied 메서드가 두번 호출되면 true를 반환하는 버그 발견

--- src/main/java/com/example/object/domain/chapter6/EventMain.java
+++ src/main/java/com/example/object/domain/chapter6/EventMain.java
@@ -1,5 +1,7 @@
 package com.example.object.domain.chapter6;
 
+import org.springframework.util.Assert;
+
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -7,8 +9,11 @@ import java.time.LocalTime;
 
 public class EventMain {
     public static void main(String[] args) {
-        Event meeting = new Event("회의", LocalDateTime.of(2019, 5, 8, 10, 30), Duration.ofMinutes(30L));
+        Event meeting = new Event("회의", LocalDateTime.of(2019, 5, 9, 10, 30), Duration.ofMinutes(30L));
 
         RecurringSchedule schedule = new RecurringSchedule("회의", DayOfWeek.WEDNESDAY, LocalTime.of(10, 30), Duration.ofMinutes(30L));
+
+        Assert.isTrue(!meeting.isSatisfied(schedule));
+        Assert.isTrue(meeting.isSatisfied(schedule));
     }
 }
