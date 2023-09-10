b10cd2e68aa630be224392923e1122b6e0781c17
Yohwan Lim <dyghks7102@naver.com>
2023-09-10 22:46:33 +0900

    feat. ServiceLocator는 의존성을 해결할 수 잇는 가장 쉬운 도구 처럼 보이지만 가장 큰 단점은 의존성을 감추게 됨 의존성을 감추게되면 에러가 컴파일타임이 아닌 런타임에 가서야 발견됨 그러므로 디버깅이 어려워짐 의존성은 명시적으로 표현하는게 좋음

---
+++ src/main/java/com/example/object/domain/chapter9/ServiceLocator.java
@@ -0,0 +1,16 @@
+package com.example.object.domain.chapter9;
+
+public class ServiceLocator {
+    private static ServiceLocator soleInstance = new ServiceLocator();
+    private DiscountPolicy discountPolicy;
+
+    public static DiscountPolicy discountPolicy() {
+        return soleInstance.discountPolicy;
+    }
+
+    public static void provide(DiscountPolicy discountPolicy) {
+        soleInstance.discountPolicy = discountPolicy;
+    }
+
+    private ServiceLocator() {}
+}
