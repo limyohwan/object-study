cc1ff9b845faa1764b97a77afbcc38b9860827fb
Yohwan Lim <dyghks7102@naver.com>
2023-09-10 21:56:18 +0900

    feat. 자주 사용하는 부분을 생성자 체이닝을 통해 기본 생성자로 만들어 놓고 만약 가끔 사용하는 discountpolicy의 경우 새로 생성자에서 받을 수 있게 만듬 클래스의 사용성을 향상시키며 다양한 컨텍스트에서 유연하게 사용될 여지 제공

--- src/main/java/com/example/object/domain/chapter8/Movie.java
+++ src/main/java/com/example/object/domain/chapter8/Movie.java
@@ -1,12 +1,22 @@
 package com.example.object.domain.chapter8;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
+import java.time.LocalTime;
 
 public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee) {
+        this(title, runningTime, fee, new AmountDiscountPolicy(Money.wons(800L),
+                new SequenceCondition(1),
+                new SequenceCondition(10),
+                new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
+                new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))));
+    }
 
     public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
         this.title = title;
