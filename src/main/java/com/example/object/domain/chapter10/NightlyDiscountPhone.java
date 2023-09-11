54edfaa2e197d934966139d00e27665a51c8c46e
limyohwan <dyghks7102@naver.com>
2023-09-11 11:32:29 +0900

    feat. 상속을 이용한 중복제거 상속을 염두에 두고 설계되지 않은 클래스를 상속을 이용해 재사용하는 것은 쉽지않음 calculateFee라는 메서드를 (40초/10초*5원)+(50/10초*2원)= 30원 계산할 것이라고 예상했지만 부모 클래스의 calculateFee라는 메서드를 활용하기 위해 ( 40초/10초*5원)+(50초/10초*5원)- (50초/10초*(5원-2원))=30원 위와 같은 식으로 계산을 하게됨 이렇게 될시 코드를 이해하기 어려워짐 상속 계층 단계를 거칠수록 더욱더 어려워지게됨 따라서 상속은 결합도를 높이며 강한 결합도는 코드를 수정하기 어렵게 만듬

---
+++ src/main/java/com/example/object/domain/chapter10/NightlyDiscountPhone.java
@@ -0,0 +1,29 @@
+package com.example.object.domain.chapter10;
+
+import java.time.Duration;
+
+public class NightlyDiscountPhone extends Phone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        super(regularAmount, seconds);
+        this.nightlyAmount = nightlyAmount;
+    }
+
+    @Override
+    public Money calculateFee() {
+        //부모 클래스의 calculateFee 호출
+        Money result = super.calculateFee();
+
+        Money nightlyFee = Money.ZERO;
+        for (Call call : getCalls()) {
+            if(call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                nightlyFee = nightlyFee.plus(getAmount().minus(nightlyAmount).times(call.getDuration().getSeconds() / getSeconds().getSeconds()));
+            }
+        }
+
+        return result.minus(nightlyFee);
+    }
+}
