ff5a892547b5281ec9d1ba8e2ebb213d516c0d43
limyohwan <dyghks7102@naver.com>
2023-09-11 14:29:51 +0900

    feat. 새로운 버전을 위한 기존 버전 저장

---
+++ src/main/java/com/example/object/domain/chapter10/OldPhone2.java
@@ -0,0 +1,53 @@
+package com.example.object.domain.chapter10;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OldPhone2 {
+    private Money amount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+    private double taxRate;
+
+    public OldPhone2(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    public OldPhone2(Money amount, Duration seconds, double taxRate) {
+        this.amount = amount;
+        this.seconds = seconds;
+        this.taxRate = taxRate;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+        }
+
+        return result.plus(result.times(taxRate));
+    }
+}
