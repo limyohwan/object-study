d8f7b85707fe7d4dba56bdd8b4cfa3c2e20ce1e9
limyohwan <dyghks7102@naver.com>
2023-09-11 11:14:17 +0900

    feat. 세금을 위한 세율 추가 및 요금 계산에 세금 더하기 중복코드는 항상 함께 수정해야함 Phone에만 적용하고 NightlyDiscountPhone에는 적용안할시 에러 발생 중복코드는 실수를 유발함 -> NightlyDiscountPhone에는 plus로 세금을 더하는 대신 minus를 하였는데 눈치채기 쉽지않음 또한 중복코드는 새로운 중복코드를 불러들이고 이는 코드의 일관성을 무너뜨릴 위험이 큼

--- src/main/java/com/example/object/domain/chapter10/Phone.java
+++ src/main/java/com/example/object/domain/chapter10/Phone.java
@@ -8,10 +8,17 @@ public class Phone {
     private Money amount;
     private Duration seconds;
     private List<Call> calls = new ArrayList<>();
+    private double taxRate;
 
     public Phone(Money amount, Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
+    }
+
+    public Phone(Money amount, Duration seconds, double taxRate) {
+        this.amount = amount;
+        this.seconds = seconds;
+        this.taxRate = taxRate;
     }
 
     public void call(Call call) {
@@ -37,6 +44,6 @@ public class Phone {
             result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
         }
 
-        return result;
+        return result.plus(result.times(taxRate));
     }
 }
