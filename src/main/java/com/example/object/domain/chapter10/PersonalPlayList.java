1237993e6d3c1501f43b8c48a0b503fab877e4c7
limyohwan <dyghks7102@naver.com>
2023-09-11 12:03:19 +0900

    feat. 가수별 노래제목도 따로 관리해달라는 새로운 요구사항이 들어왔을경우 자식 클래스도 변경을 해야함

--- src/main/java/com/example/object/domain/chapter10/PersonalPlayList.java
+++ src/main/java/com/example/object/domain/chapter10/PersonalPlayList.java
@@ -3,5 +3,6 @@ package com.example.object.domain.chapter10;
 public class PersonalPlayList extends PlayList {
     public void remove(Song song) {
         getTracks().remove(song);
+        getSingers().remove(song.getSinger());
     }
 }
