c265f2a8b1f006621b4904b272b71476ef3f4dea
Yohwan Lim <dyghks7102@naver.com>
2023-10-03 20:03:23 +0900

    feat. 상속을 이용하여 GradeLecture 클래스 생성 메서드 오버로딩 average

---
+++ src/main/java/com/example/object/domain/chapter12/GradeLecture.java
@@ -0,0 +1,65 @@
+package com.example.object.domain.chapter12;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class GradeLecture extends Lecture {
+    private List<Grade> grades;
+
+    public GradeLecture(int pass, String title, List<Integer> scores, List<Grade> grades) {
+        super(pass, title, scores);
+        this.grades = grades;
+    }
+
+    @Override
+    public String evaluate() {
+        return super.evaluate() + ", " + gradeStatistics();
+    }
+
+    private String gradeStatistics() {
+        return grades.stream()
+                .map(grade -> format(grade))
+                .collect(joining(" "));
+    }
+
+    private String format(Grade grade) {
+        return String.format("%s : %d", grade.getName(), gradeCount(grade));
+    }
+
+    private long gradeCount(Grade grade) {
+        return getScores().stream()
+                .filter(grade::include)
+                .count();
+    }
+
+    public double average(String gradeName) {
+        return grades.stream()
+                .filter(each -> each.isName(gradeName))
+                .findFirst()
+                .map(this::gradeAverage)
+                .orElse(0d);
+    }
+
+    private double gradeAverage(Grade grade) {
+        return getScores().stream()
+                .filter(grade::include)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+    }
+
+    public static void main(String[] args) {
+        GradeLecture lecture = new GradeLecture(70,
+                "객체지향 프로그래밍",
+                List.of(81, 95, 75, 50, 45),
+                List.of(new Grade("A", 100, 95),
+                        new Grade("B", 94, 80),
+                        new Grade("C", 79, 70),
+                        new Grade("D", 69, 50),
+                        new Grade("F", 49, 0)));
+
+        System.out.println(lecture.evaluate());
+        System.out.println(lecture.average("A"));
+    }
+}
