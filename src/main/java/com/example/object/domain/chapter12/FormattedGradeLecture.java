c7600cd08d8bbdcf0615ad1faefc5f5b8c5baeea
Yohwan Lim <dyghks7102@naver.com>
2023-10-03 20:16:30 +0900

    feat. super참조의 용도 super 참조의 정확한 의도는 지금 이 클래스의 부모 클래스에서부터 메서드 탐색을 시작하시요 임 만약 상위 부모클래스에서 메서드가 존재하지 않으면 더 상위로 이동하여 찾음

---
+++ src/main/java/com/example/object/domain/chapter12/FormattedGradeLecture.java
@@ -0,0 +1,14 @@
+package com.example.object.domain.chapter12;
+
+import java.util.List;
+
+public class FormattedGradeLecture extends GradeLecture {
+
+    public FormattedGradeLecture(int pass, String title, List<Integer> scores, List<Grade> grades) {
+        super(pass, title, scores, grades);
+    }
+
+    public String formatAverage() {
+        return String.format("Avg: %1.1f", super.average());
+    }
+}
