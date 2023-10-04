becef62497208b22b5123f537127306c64e68b64
Yohwan Lim <dyghks7102@naver.com>
2023-10-04 23:12:18 +0900

    feat. 정사각형은 직사각형이지만 둘의 관계는 서브타이핑 관계가 아니라 서브 클래싱 관계임 is-a의 관계는 클라이언트 관점에서 행동이 호환되어야함

--- src/main/java/com/example/object/domain/chapter13/Client.java
+++ src/main/java/com/example/object/domain/chapter13/Client.java
@@ -10,4 +10,15 @@ public class Client {
     public void flyBird(FlyingBird bird) {
         bird.fly();
     }
+
+    public void resize(Rectangle rectangle, int width, int height) {
+        rectangle.setHeight(width);
+        rectangle.setHeight(height);
+        assert rectangle.getWidth() == width && rectangle.getHeight() == height;
+    }
+
+    public void resize() {
+        Square square = new Square(10, 10, 10);
+        resize(square, 50, 100);
+    }
 }
