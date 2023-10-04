3757924ec9123b5315cee6499f28742750fef1be
Yohwan Lim <dyghks7102@naver.com>
2023-10-04 22:49:11 +0900

    feat. 펭귄은 fly시 에러를 던짐

--- src/main/java/com/example/object/domain/chapter13/Penguin.java
+++ src/main/java/com/example/object/domain/chapter13/Penguin.java
@@ -1,6 +1,10 @@
 package com.example.object.domain.chapter13;
 
 public class Penguin extends Bird {
+//    @Override
+//    public void fly() {}
     @Override
-    public void fly() {}
+    public void fly() {
+        throw new UnsupportedOperationException();
+    }
 }
