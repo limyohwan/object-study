2078896be6768996b6b9c067e379b45bdfa87703
Yohwan Lim <dyghks7102@naver.com>
2023-10-05 22:49:38 +0900

    feat. 서브타입에 더 강력한 사전조건을 정의할 수 없다 자식 클래스는 부모클래스의 서브타입이 되어야하기 때문에 부모의 사전조건 정도의 레벨로만 만족할 수 있어야함

---
+++ src/main/java/com/example/object/domain/chapter13/BrokenDiscountPolicy.java
@@ -0,0 +1,41 @@
+package com.example.object.domain.chapter13;
+
+import com.example.object.domain.chapter9.DiscountCondition;
+import com.example.object.domain.chapter9.Money;
+import com.example.object.domain.chapter9.Screening;
+
+import java.time.LocalTime;
+
+public class BrokenDiscountPolicy extends DiscountPolicy {
+
+    public BrokenDiscountPolicy(DiscountCondition... conditions) {
+        super(conditions);
+    }
+
+    @Override
+    public Money calculateDiscountAmount(Screening screening) {
+        checkPreCondition(screening); // 기존의 사전조건
+        checkStrongerPreCondition(screening); // 더 강력한 사전조건
+
+        Money amount = screening.getMovieFee();
+        checkPostCondition(amount); // 기존의 사후조건
+        return amount;
+    }
+
+    private void checkWeakerPostCondition(Money amount) {
+        assert amount != null;
+    }
+
+    private void checkStrongerPreCondition(Screening screening) {
+        assert screening.getStartTime().toLocalTime().isBefore(LocalTime.MIDNIGHT);
+    }
+
+    private void checkStrongerPostCondition(Money amount) {
+        assert amount.isGreaterThanOrEqual(Money.wons(1000));
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
